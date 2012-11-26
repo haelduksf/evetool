c78b8cbfb63ee72787611912cddca57f5a3367f6
Alec Ross <haelduksf@gmail.com>
2012-11-25 21:33:39 -0500

    Linkified  neighbour list
    Includes some refactoring to remove JSTL "forEach"es that interfered
    with commandlinks.

--- webClient/src/main/java/com/hael/beans/SolarSystemDetailsBean.java
+++ webClient/src/main/java/com/hael/beans/SolarSystemDetailsBean.java
@@ -3,16 +3,22 @@ package com.hael.beans;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.ChartSeries;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.hael.wsClient.ActivityLog;
+import com.hael.wsClient.IOException_Exception;
 import com.hael.wsClient.JumpCalculator;
 import com.hael.wsClient.JumpCalculatorImplService;
 import com.hael.wsClient.MapSolarSystemJumpLog;
@@ -28,15 +34,15 @@ import com.hael.wsClient.SolarSystem;
  * 
  */
 @ManagedBean
-@RequestScoped
+@ViewScoped
 public class SolarSystemDetailsBean {
 
-	private static final int DEFAULT_GRAPH_TIME = -24 ; // 24
-																				// hours
+	private static final int DEFAULT_GRAPH_TIME = -24 ; // 24 hours
 	private SolarSystem solarSystem;
 	private JumpCalculatorImplService service = new JumpCalculatorImplService();
 	private JumpCalculator jumpCalculator = service.getJumpCalculatorImplPort();
-	
+	private static final Logger log = LoggerFactory
+			.getLogger(SolarSystemDetailsBean.class);
 	
 
 	public SolarSystemDetailsBean() {
@@ -102,5 +108,22 @@ public class SolarSystemDetailsBean {
 	public void setSolarSystem(SolarSystem solarSystem) {
 		this.solarSystem = solarSystem;
 	}
-
+	
+	/**
+	 * Convenience method to set solar system by name rather than reference
+	 * @param solarSystem the name of the solarSystem to set
+	 * @throws IOException_Exception 
+	 */
+	public void setSolarSystemByName(String solarSystem) throws IOException_Exception {
+		log.debug("Setting solar system to {}", solarSystem);
+				
+		try {
+			this.solarSystem = jumpCalculator.getSystem(solarSystem);
+		} catch (NotARealSolarSystemException e) {
+			FacesMessage error = new FacesMessage(e.getMessage());
+			error.setSeverity(FacesMessage.SEVERITY_ERROR);
+			FacesContext.getCurrentInstance().addMessage(null, error);
+		}
+		log.debug("solarSystem changed to {}", this.solarSystem.getName());
+	}
 }
